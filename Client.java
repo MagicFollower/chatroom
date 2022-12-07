a38e4cde5277adb08fe0a3c06d9a4c304ec897ea
trivis <webturing256@163.com>
2022-12-07 21:26:37 +0800

    add server&client

---
+++ Client.java
@@ -0,0 +1,74 @@
+package com.job.day23.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Client {
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private final ByteBuffer buffer = ByteBuffer.allocate(512);
+
+    public Client() {
+        try {
+            final int PORT = 6666;
+            selector = Selector.open();
+            socketChannel = SocketChannel.open(new InetSocketAddress(PORT));
+            // socketChannel#read是否会阻塞（设置为false）
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
+        } catch (Exception e) {
+            throw new RuntimeException("无法连接至服务器，请稍后再试。");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        new Thread(() -> {
+            for (; ; ) {
+                client.readData();
+            }
+        }, "th1").start();
+
+        // 读取控制台输入
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()) {
+            String s = scanner.nextLine().trim();
+            client.sendData(s);
+        }
+    }
+
+    public void readData() {
+        try {
+            if(selector.select()>0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                if(iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    if(selectionKey.isReadable()) {
+                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel().configureBlocking(false);
+                        socketChannel.configureBlocking(false);
+                        int read = socketChannel.read(buffer.clear());
+                        if (read > 0) {
+                            System.out.println(new String(buffer.array(), 0, read).trim());
+                        }
+                    }
+                    iterator.remove();
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("与远程服务器失去连接，请稍后再试。");
+        }
+    }
+
+    public void sendData(String msg) {
+        try {
+            socketChannel.write(buffer.clear().put(msg.getBytes()).flip());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,135 @@
+package com.job.day23.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.*;
+
+public class Server {
+
+    private final ByteBuffer buffer = ByteBuffer.allocate(512);
+    private ExecutorService THREAD_POOL;
+    private Selector selector;
+    private ServerSocketChannel serverSocketChannel;
+    private Queue<SocketChannel> blockingQueue;
+    private List<SocketChannel> onlineSocket;
+
+    public Server() {
+        try {
+            final int PORT = 6666;
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open().bind(new InetSocketAddress(PORT));
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            final int ACTIVE_THREAD_COUNT = 2;
+            THREAD_POOL = new ThreadPoolExecutor(
+                    ACTIVE_THREAD_COUNT,
+                    ACTIVE_THREAD_COUNT,
+                    0,
+                    TimeUnit.SECONDS,
+                    new SynchronousQueue<>()
+            );
+            blockingQueue = new ArrayBlockingQueue<>(100);
+            onlineSocket = new CopyOnWriteArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.listen();
+    }
+
+    public void listen() throws IOException {
+        while (true) {
+            if (selector.select(2000) > 0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                if (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    if (selectionKey.isAcceptable()) {
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        // todo 记录用户上线 (finished)
+                        onlineSocket.add(socketChannel);
+                        System.out.println(socketChannel.getRemoteAddress() + "上线!");
+                        // 将socketChannel放入阻塞队列
+                        blockingQueue.add(socketChannel);
+                        // todo 将读操作分发给线程池中线程进行处理 (finished)
+                        if (((ThreadPoolExecutor) THREAD_POOL).getActiveCount() != 2) {
+                            CompletableFuture.runAsync(() -> {
+                                System.out.println(Thread.currentThread().getName() + ": 接收任务！\uD83D\uDE80");
+                                try {
+                                    Selector selector = Selector.open();
+                                    for (; ; ) {
+                                        if (blockingQueue.peek() != null) {
+                                            SocketChannel item = blockingQueue.poll();
+                                            item.configureBlocking(false);
+                                            item.register(selector, SelectionKey.OP_READ);
+                                        }
+                                        // bug → channel没有close()时异常断开连接，select会出现异常循环！
+                                        // server端监控到客户端异常断开，需要触发主动关闭操作（客户端断开后如果channel依然open）
+                                        // ↑ 这种情况会出现 → 如果直接运行客户端，然后强制关闭程序（客户端关闭时没有主动调用close）
+                                        if (selector.select(2000) > 0) {
+                                            // todo check
+                                            System.out.println(selector.selectedKeys().size());
+                                            Iterator<SelectionKey> ri = selector.selectedKeys().iterator();
+                                            if (ri.hasNext()) {
+                                                SelectionKey next = ri.next();
+                                                if (next.isReadable()) {
+                                                    SocketChannel channel = (SocketChannel) next.channel();
+                                                    ri.remove();
+                                                    try {
+                                                        int read = channel.read(buffer.clear());
+                                                        String msg = channel.getRemoteAddress().toString().substring(1)
+                                                                + ": " +
+                                                                new String(buffer.array(), 0, read, StandardCharsets.UTF_8);
+                                                        if (read > 0) {
+                                                            // System.out.println("读取到客户端(" +
+                                                            //         channel.getRemoteAddress().toString() + ")数据：" + msg);
+                                                            // todo 转发客户端数据给所有在线用户（finished）
+                                                            resendMsgToAll(msg, channel);
+                                                        } else {
+                                                            System.out.println((channel.getRemoteAddress() + "离线\uD83D\uDE80!"));
+                                                            // todo server主动关闭连接 (finished)
+                                                            if (channel.isOpen()) channel.close();
+                                                            onlineSocket.remove(channel);
+                                                        }
+                                                    } catch (IOException e) {
+                                                        System.out.println((channel.getRemoteAddress() + "离线\uD83D\uDE80!!"));
+                                                        if (channel.isOpen()) channel.close();
+                                                        onlineSocket.remove(channel);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                } catch (IOException e) {
+                                    System.out.println(e.getMessage());
+                                }
+                            }, THREAD_POOL);
+                        }
+                    }
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+
+    public void resendMsgToAll(String msg, SocketChannel self) throws IOException {
+        buffer.clear().put(msg.getBytes());
+        for (SocketChannel u : onlineSocket) {
+            if (u != self) {
+                u.write(buffer.flip());
+            }
+        }
+    }
+}
